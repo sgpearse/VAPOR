3cef144ad980c7c11ee9f6424dc810a415b2501e
Stanislaw Jaroszynski <stasj@ucar.edu>
2019-07-26 14:18:57 -0600

    inc

--- include/vapor/VolumeParams.h
+++ include/vapor/VolumeParams.h
@@ -79,6 +79,9 @@ private:
     static const std::string _phongDiffuseTag;
     static const std::string _phongSpecularTag;
     static const std::string _phongShininessTag;
+
+public:
+    static const std::string OSPRaySamplingRateTag;
 };
 
 };    // namespace VAPoR
