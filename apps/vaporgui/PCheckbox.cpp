34fab6e7e9f4f758431d069e9135531930a25dd1
Stanislaw Jaroszynski <Stanislaw Jaroszynski>
2019-10-24 15:20:57 -0600

    PCheckbox

--- apps/vaporgui/PCheckbox.cpp
+++ apps/vaporgui/PCheckbox.cpp
@@ -1,4 +1,13 @@
 #include "PCheckbox.h"
 #include "VLineItem.h"
+#include <vapor/ParamsBase.h>
 
-PCheckbox::PCheckbox(const std::string &tag, const std::string &label) : PWidget(tag) {}
+PCheckbox::PCheckbox(const std::string &tag, const std::string &label) : PWidget(tag, new VLineItem(label == "" ? tag : label, _qcheckbox = new QCheckBox)) {}
+
+void PCheckbox::update() const
+{
+    bool on = getParams()->GetValueLong(GetTag(), 0);
+    _qcheckbox->setChecked(on);
+}
+
+void PCheckbox::checkboxStateChanged(int state) { getParams()->SetValueLong(GetTag(), GetTag(), state == Qt::CheckState::Checked); }
