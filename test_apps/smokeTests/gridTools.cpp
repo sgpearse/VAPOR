67ec49ff9a42337de1530eca0b191f2c74abef31
Scott Pearse <pearse@ucar.edu>
2020-03-23 16:39:28 -0600

    removed diagnostic

--- test_apps/smokeTests/gridTools.cpp
+++ test_apps/smokeTests/gridTools.cpp
@@ -152,7 +152,6 @@ void CompareIndexToCoords(VAPoR::Grid *grid,
         yNudge = (maxu[Y] - minu[Y]) / (y * 10);
         zNudge = (maxu[Z] - minu[Z]) / (z * 10);
     }
-    cout << "                   " << xNudge << " " << yNudge << " " << zNudge << endl;
 
     double peak = 0.f;
 
@@ -247,6 +246,9 @@ void TestGrid(Grid *grid, const std::vector<std::string> &tests, float minVal, f
         grid->SetInterpolationOrder(nearestNeighbor);
         CompareIndexToCoords(grid, rms, numMissingValues, disagreements);
         cout << "  nearestNeighbor: " << endl;
+        PrintStats(rms, numMissingValues, disagreements);
+        CompareIndexToCoords(grid, rms, numMissingValues, disagreements);
+        cout << "  nearestNeighbor (nudged): " << endl;
         PrintStats(rms, numMissingValues, disagreements);
     }
 
