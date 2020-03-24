0b80646e4978fced9e136b02759b6133b78496d3
Scott Pearse <pearse@ucar.edu>
2020-03-24 15:03:39 -0600

    intermediate

--- test_apps/smokeTests/gridTools.h
+++ test_apps/smokeTests/gridTools.h
@@ -16,8 +16,8 @@ void MakeRamp(VAPoR::Grid *grid, float minVal, float maxVal, size_t axis);
 void CompareIndexToCoords(VAPoR::Grid *grid,
                           double &     rms,                 // Root Mean Square error
                           size_t &     numMissingValues,    // Counter for receiving MissingValue upon query
-                          size_t &     disagreements,       // Counter for when AccessIJK() and GetValue() disagree
-                          bool         nudge = false);
+                          size_t &     disagreements        // Counter for when AccessIJK() and GetValue() disagree
+);
 
 void TestNodeIterator(const VAPoR::Grid *g, int &count, double &time);
 
