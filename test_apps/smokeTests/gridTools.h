ab3694d53f85d26592b26290fbaccc328232ac05
Scott Pearse <pearse@ucar.edu>
2020-04-09 11:31:07 -0600

    Incremental

--- test_apps/smokeTests/gridTools.h
+++ test_apps/smokeTests/gridTools.h
@@ -11,19 +11,24 @@ void MakeTriangle(VAPoR::Grid *grid, float minVal, float maxVal);
 
 void MakeConstantField(VAPoR::Grid *grid, float value);
 
-void MakeRamp(VAPoR::Grid *grid, float minVal, float maxVal, size_t axis);
+void MakeRamp(VAPoR::Grid *grid, float minVal, float maxVal);
 
-void CompareIndexToCoords(VAPoR::Grid *grid,
-                          double &     rms,                 // Root Mean Square error
-                          size_t &     numMissingValues,    // Counter for receiving MissingValue upon query
-                          size_t &     disagreements        // Counter for when AccessIJK() and GetValue() disagree
+void MakeRampOnAxis(VAPoR::Grid *grid, float minVal, float maxVal, size_t axis);
+
+int CompareIndexToCoords(VAPoR::Grid *grid,
+                         double &     rms,                 // Root Mean Square error
+                         size_t &     numMissingValues,    // Counter for receiving MissingValue upon query
+                         size_t &     disagreements        // Counter for when AccessIJK() and GetValue() disagree
 );
 
-void TestNodeIterator(const VAPoR::Grid *g, int &count, double &time);
+// Returns the expected node count for the grid
+size_t TestNodeIterator(const VAPoR::Grid *g, int &count, double &time);
 
 void PrintStats(double rms, size_t numMissingValues, size_t disagreements);
 
-void TestGrid(VAPoR::Grid *grid, const std::vector<std::string> &tests, float minVal, float maxVal);
+int RunTest(VAPoR::Grid *grid);
+
+int RunTests(VAPoR::Grid *grid, const std::vector<std::string> &tests, float minVal, float maxVal);
 
 VAPoR::StretchedGrid *MakeStretchedGrid(const std::vector<size_t> &dims, const std::vector<size_t> &bs, const std::vector<double> &minu, const std::vector<double> &maxu);
 
