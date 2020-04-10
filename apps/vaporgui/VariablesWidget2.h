5020b1a5271cd62cfcaa1ecb67b048f6d11a7f99
Scott Pearse <pearse@ucar.edu>
2020-04-10 13:22:16 -0600

    incremental

---
+++ apps/vaporgui/VariablesWidget2.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "PVariableSelectorHLI.h"
+#include "PVariableSelector.h"
+#include "VSection.h"
+#include "Flags.h"
+
+class PStringDowndownHLI;
+
+namespace VAPoR {
+class DataMgr;
+class ParamsMgr;
+class RenderParams;
+}    // namespace VAPoR
+
+class VariablesWidget2 : public VSection {
+    Q_OBJECT
+
+public:
+    VariablesWidget2();
+
+    void Reinit(VariableFlags varFlags, DimFlags dimFlags);
+
+    void Update(VAPoR::DataMgr *dataMgr, VAPoR::ParamsMgr *paramsMgr, VAPoR::RenderParams *rParams);
+
+private:
+    PStringDropdown *_dimSelector;
+
+    PVariableSelector *    _scalarVar;
+    PFieldVariableSelector _fieldVars;
+    PVariableSelector *    _colorVar;
+    PVariableSelector *    _heightVar;
+};
