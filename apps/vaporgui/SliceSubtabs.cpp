93780172f350b8d16db9cfa4db5172c6c5ec34f6
Scott Pearse <pearse@ucar.edu>
2020-04-13 13:29:45 -0600

    copying ParamsWidgetDemo as much as possible

--- apps/vaporgui/SliceSubtabs.cpp
+++ apps/vaporgui/SliceSubtabs.cpp
@@ -28,7 +28,7 @@ SliceVariablesSubtab::SliceVariablesSubtab(QWidget *parent)
     connect(refinementCombo, SIGNAL(currentIndexChanged(int)), this, SLOT(_setDefaultSampleRate()));
 
     layout()->addWidget(_pg = new PGroup);
-    _pg->Add(new PEnumDropdownHLI<RenderParams>("PEnumDropdownHLI ( Refinement )", {"2", "3"}, {2, 3}, &RenderParams::GetRefinementLevel(), &RenderParams::SetRefinementLevel()));
+    _pg->Add(new PEnumDropdownHLI<RenderParams>("PEnumDropdownHLI ( Refinement )", {"2", "3"}, {2, 3}, &RenderParams::GetRefinementLevel, &RenderParams::SetRefinementLevel));
 }
 
 void SliceVariablesSubtab::Update(VAPoR::DataMgr *dataMgr, VAPoR::ParamsMgr *paramsMgr, VAPoR::RenderParams *rParams)
