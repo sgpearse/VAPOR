6eab744786ade669b51cb2cfca18b3468c377436
Scott Pearse <pearse@ucar.edu>
2020-04-13 11:49:31 -0600

    tmp

--- apps/vaporgui/PVariableSelectorHLI.h
+++ apps/vaporgui/PVariableSelectorHLI.h
@@ -12,7 +12,7 @@
 template<class P> class PVariableSelectorHLI : public PVariableSelector, public PWidgetHLI<P> {
 public:
     PVariableSelectorHLI(const std::string &label, typename PWidgetHLIBase<P, std::string>::GetterType getter, typename PWidgetHLIBase<P, std::string>::SetterType setter)
-    : PVariableSelector<P>(label, {}, getter, setter), PWidgetHLIBase<P, std::string>((PWidget *)this, getter, setter)
+    : PVariableSelector<P>("", label), PWidgetHLIBase<P, std::string>((PWidget *)this, getter, setter)
     {
     }
 };
