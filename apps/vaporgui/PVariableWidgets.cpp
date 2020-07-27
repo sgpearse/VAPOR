fecc1ad074c66aad624ce980d32f6b88519d4a7c
Scott Pearse <pearse@ucar.edu>
2020-07-27 10:17:34 -0600

    Flow dimensionality selector is buggy

--- apps/vaporgui/PVariableWidgets.cpp
+++ apps/vaporgui/PVariableWidgets.cpp
@@ -1,5 +1,7 @@
 #include "PVariableWidgets.h"
 #include <vapor/RenderParams.h>
+#include <vapor/ParamsMgr.h>
+#include "GUIStateParams.h"
 #include "VComboBox.h"
 #include "PSection.h"
 #include "PFidelitySection.h"
@@ -31,16 +33,31 @@ void PDimensionSelector::dropdownTextChanged(std::string text)
     RenderParams *rp = (RenderParams *)getParams();
     int           dim = text == "2D" ? 2 : 3;
 
-    rp->BeginGroup("Change dim");
+    /*    rp->BeginGroup("Change dim");
     if (dim == 2) {
         rp->GetBox()->SetPlanar(true);
         rp->GetBox()->SetOrientation(VAPoR::Box::XY);
-    } else {
+    }
+    else {
         rp->GetBox()->SetPlanar(false);
         rp->GetBox()->SetOrientation(VAPoR::Box::XYZ);
-    }
+    }*/
     rp->SetDefaultVariables(dim, true);
-    rp->EndGroup();
+    // rp->EndGroup();
+}
+
+PFlowDimensionSelector::PFlowDimensionSelector() : PDimensionSelector() {}
+
+void PFlowDimensionSelector::dropdownTextChanged(std::string text)
+{
+    int dim = text == "2D" ? 2 : 3;
+
+    ParamsMgr *     pm = getParamsMgr();
+    GUIStateParams *gp;
+    gp = dynamic_cast<GUIStateParams *>(pm->GetParams(GUIStateParams::GetClassType()));
+    gp->SetFlowDimensionality(dim);
+
+    PDimensionSelector::dropdownTextChanged(text);
 }
 
 // ==================================
